40ea9150e20bbca0cebb285202852bfd217f6698
incuso <numismatica@gmail.com>
2023-10-04 09:51:19 +0200

    pessima idea
    Signed-off-by: incuso <numismatica@gmail.com>

--- simple/parseopt.h
+++
@@ -1,7 +0,0 @@
-struct ArgumentList {
-  std::string image_name;                   //!< image file name
-  int wait_t;                               //!< waiting time
-};
-
-bool ParseInputs(ArgumentList& args, int argc, char **argv);
-
--- simple/simple.cpp
+++ simple/simple.cpp
@@ -9,7 +9,14 @@
 #include <string>
 
 //options
-#include "parseopt.h"
+
+struct ArgumentList {
+  std::string image_name;                   //!< image file name
+  int wait_t;                               //!< waiting time
+};
+
+bool ParseInputs(ArgumentList& args, int argc, char **argv);
+
 
 int main(int argc, char **argv)
 {
@@ -98,4 +105,59 @@ int main(int argc, char **argv)
 
   return 0;
 }
+
+
+bool ParseInputs(ArgumentList& args, int argc, char **argv);
+
+#if 0
+bool ParseInputs(ArgumentList& args, int argc, char **argv) {
+  args.wait_t=0;
+
+  cv::CommandLineParser parser(argc, argv,
+      "{input   i|in.png|input image, Use %0xd format for multiple images.}"
+      "{wait    t|0     |wait before next frame (ms)}"
+      "{help    h|<none>|produce help message}"
+      );
+
+  if(parser.has("help"))
+  {
+    parser.printMessage();
+    return false;
+  }
+
+  args.image_name = parser.get<std::string>("input");
+  args.wait_t     = parser.get<int>("wait");
+
+  return true;
+}
+#else
+
+#include <unistd.h>
+bool ParseInputs(ArgumentList& args, int argc, char **argv) {
+  int c;
+
+  while ((c = getopt (argc, argv, "hi:t:")) != -1)
+    switch (c)
+    {
+      case 't':
+        args.wait_t = atoi(optarg);
+        break;
+      case 'i':
+        args.image_name = optarg;
+        break;
+      case 'h':
+      default:
+        std::cout<<"usage: " << argv[0] << " -i <image_name>"<<std::endl;
+        std::cout<<"exit:  type q"<<std::endl<<std::endl;
+        std::cout<<"Allowed options:"<<std::endl<<
+          "   -h                       produce help message"<<std::endl<<
+          "   -i arg                   image name. Use %0xd format for multiple images."<<std::endl<<
+          "   -t arg                   wait before next frame (ms)"<<std::endl<<std::endl;
+        return false;
+    }
+  return true;
+}
+
+#endif
+
 
